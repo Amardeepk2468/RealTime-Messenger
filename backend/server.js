9b645878cb04f89a9173e00cd50c878f8bbff144
Amardeep <121479647+Amardeepk2468@users.noreply.github.com>
2024-08-03 02:14:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const io=require('socket.io')(
   server,{
     pingTimeout:60000,
     cors:{
-    origin:"http://localhost:3000",
+    origin:"https://real-time-messenger-green.vercel.app",
     } 
   }
 )
